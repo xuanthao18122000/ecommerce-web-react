f7f119ee0bc940a18a337eb583e3911d20ddddd0
Thao <xuanthao.nguyen>
2023-07-05 10:21:53 +0700

    add header and footer ui

--- src/components/layouts/footer.layout.tsx
+++ src/components/layouts/footer.layout.tsx
@@ -1,7 +1,255 @@
-
+import '../../styles/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCreditCard, faTruck, faMoneyCheckDollar, faComments } from '@fortawesome/free-solid-svg-icons'
 
 export default function FooterScreen() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer id="footer" className="footer-2">
+      <div className="store-feature section">
+        	<div className="container">
+            	<div className="row">
+                	<div className="col-12 col-sm-12 col-md-12 col-lg-12">
+                    	<ul className="display-table store-info">
+                        	<li className="display-table-cell text-center">
+                              <div className="text-center d-flex w-100 h-50 justify-content-center"><FontAwesomeIcon icon={faTruck} className="icon w-25"/></div>
+                            	<h5>Free Shipping &amp; Return</h5>
+                            	<span className="sub-text">Free shipping on all US orders</span>
+                            </li>
+                          	<li className="display-table-cell">
+                            <div className="text-center d-flex w-100 h-50 justify-content-center"><FontAwesomeIcon icon={faMoneyCheckDollar} className="icon w-25"/></div>
+                                <h5>Money Guarantee</h5>
+                                <span className="sub-text">30 days money back guarantee</span>
+                          	</li>
+                          	<li className="display-table-cell">
+                            <div className="text-center d-flex w-100 justify-content-center"><FontAwesomeIcon icon={faComments} className="icon w-25"/></div>
+                                <h5>Online Support</h5>
+                                <span className="sub-text">We support online 24/7 on day</span>
+                            </li>
+                          	<li className="display-table-cell">
+                            <div className="text-center d-flex w-100 justify-content-center"><FontAwesomeIcon icon={faCreditCard} className="icon w-25"/></div>
+                                <h5>Secure Payments</h5>
+                                <span className="sub-text">All payment are Secured and trusted.</span>
+                        	</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+			<div className="newsletter-section">
+				<div className="container">
+					<div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-7 w-100 d-flex justify-content-start align-items-center">
+							<div className="display-table">
+								<div className="display-table-cell footer-newsletter">
+									<div className="section-header text-center">
+										<label className="h2">
+											<span>sign up for </span>newsletter
+										</label>
+									</div>
+									<form action="#" method="post">
+										<div className="input-group">
+											<input
+												type="email"
+												placeholder="Email address"
+											/>
+											<span className="input-group__btn">
+												<button
+													type="submit"
+													className="btn newsletter__submit"
+													name="commit"
+													id="Subscribe"
+												>
+													<span className="newsletter__submit-text--large">
+														Subscribe
+													</span>
+												</button>
+											</span>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div className="col-12 col-sm-12 col-md-12 col-lg-5 d-flex justify-content-end align-items-center">
+							<div className="footer-social">
+								<ul className="list--inline site-footer__social-icons social-icons">
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Facebook"
+										>
+											<i className="icon icon-facebook"></i>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Twitter"
+										>
+											<i className="icon icon-twitter"></i>{" "}
+											<span className="icon__fallback-text">Twitter</span>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Pinterest"
+										>
+											<i className="icon icon-pinterest"></i>{" "}
+											<span className="icon__fallback-text">Pinterest</span>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Instagram"
+										>
+											<i className="icon icon-instagram"></i>{" "}
+											<span className="icon__fallback-text">Instagram</span>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Tumblr"
+										>
+											<i className="icon icon-tumblr-alt"></i>{" "}
+											<span className="icon__fallback-text">Tumblr</span>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on YouTube"
+										>
+											<i className="icon icon-youtube"></i>{" "}
+											<span className="icon__fallback-text">YouTube</span>
+										</a>
+									</li>
+									<li>
+										<a
+											className="social-icons__link"
+											href="#"
+											target="_blank"
+											title="Belle Multipurpose Bootstrap 4 Template on Vimeo"
+										>
+											<i className="icon icon-vimeo-alt"></i>{" "}
+											<span className="icon__fallback-text">Vimeo</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="site-footer">
+				<div className="container">
+					<div className="footer-top">
+						<div className="row">
+							<div className="col-12 col-sm-12 col-md-3 col-lg-3 footer-links">
+								<h4 className="h4">Quick Shop</h4>
+								<ul>
+									<li>
+										<a href="#">Women</a>
+									</li>
+									<li>
+										<a href="#">Men</a>
+									</li>
+									<li>
+										<a href="#">Kids</a>
+									</li>
+									<li>
+										<a href="#">Sportswear</a>
+									</li>
+									<li>
+										<a href="#">Sale</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-12 col-sm-12 col-md-3 col-lg-3 footer-links">
+								<h4 className="h4">Informations</h4>
+								<ul>
+									<li>
+										<a href="#">About us</a>
+									</li>
+									<li>
+										<a href="#">Careers</a>
+									</li>
+									<li>
+										<a href="#">Privacy policy</a>
+									</li>
+									<li>
+										<a href="#">Terms &amp; condition</a>
+									</li>
+									<li>
+										<a href="#">My Account</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-12 col-sm-12 col-md-3 col-lg-3 footer-links">
+								<h4 className="h4">Customer Services</h4>
+								<ul>
+									<li>
+										<a href="#">Request Personal Data</a>
+									</li>
+									<li>
+										<a href="#">FAQ's</a>
+									</li>
+									<li>
+										<a href="#">Contact Us</a>
+									</li>
+									<li>
+										<a href="#">Orders and Returns</a>
+									</li>
+									<li>
+										<a href="#">Support Center</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-12 col-sm-12 col-md-3 col-lg-3 contact-box">
+								<h4 className="h4">Contact Us</h4>
+								<ul className="addressFooter">
+									<li>
+										<i className="icon anm anm-map-marker-al"></i>
+										<p>
+											55 Gallaxy Enque,
+											<br />
+											2568 steet, 23568 NY
+										</p>
+									</li>
+									<li className="phone">
+										<i className="icon anm anm-phone-s"></i>
+										<p>(440) 000 000 0000</p>
+									</li>
+									<li className="email">
+										<i className="icon anm anm-envelope-l"></i>
+										<p>sales@yousite.com</p>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<hr />
+					<div className="footer-bottom">
+						<div className="col-12 col-sm-12 col-md-12 col-lg-12 order-1 order-md-0 order-lg-0 order-sm-1 copyright text-center">
+							<span></span> <a href="templateshub.net">Templates Hub</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
--- src/components/layouts/header.layout.tsx
+++ src/components/layouts/header.layout.tsx
@@ -1,91 +1,365 @@
-import React from 'react';
+import React from "react";
+import Logo from "../../assets/images/logo.svg";
 
 const Header = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="./index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Women
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Men
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./shop.html">
-                Shop
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Pages
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="./product-details.html">
-                    Product Details
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./shop-cart.html">
-                    Shop Cart
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./checkout.html">
-                    Checkout
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./blog-details.html">
-                    Blog Details
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./blog.html">
-                Blog
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="./contact.html">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<div className="pageWrapper">
+			<div className="top-header">
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-10 col-sm-8 col-md-5 col-lg-4">
+							<div className="language-dropdown">
+								<span className="language-dd">English</span>
+								<ul id="language">
+									<li className="">German</li>
+									<li className="">French</li>
+								</ul>
+							</div>
+							<p className="phone-no">
+								<i className="anm anm-phone-s"></i> +440 0(111) 044 833
+							</p>
+						</div>
+						<div className="col-sm-4 col-md-4 col-lg-4 d-md-block d-lg-block text-center">
+							Worldwide Express Shipping
+						</div>
+						<div className="col-2 col-sm-4 col-md-3 col-lg-4 text-right">
+							<span className="user-menu d-block d-lg-none">
+								<i className="anm anm-user-al" aria-hidden="true"></i>
+							</span>
+							<ul className="customer-links list-inline">
+								<li>
+									<a href="login.html">Login</a>
+								</li>
+								<li>
+									<a href="register.html">Create Account</a>
+								</li>
+								<li>
+									<a href="wishlist.html">Wishlist</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="header-wrap classicHeader animated d-flex">
+				<div className="container-fluid">
+					<div className="row align-items-center">
+						<div className="logo col-md-2 col-lg-2 d-none d-lg-block">
+							<a href="index.html">
+								<img
+									src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/United_technologies_logo.svg/1024px-United_technologies_logo.svg.png"
+									alt="Logo"
+									title="Logo"
+									style={{ width: "50%" }}
+								/>
+							</a>
+						</div>
+						<div className="col-2 col-sm-3 col-md-3 col-lg-8">
+							<div className="d-block d-lg-none">
+								<button
+									type="button"
+									className="btn--link site-header__menu js-mobile-nav-toggle mobile-nav--open"
+								>
+									<i className="icon anm anm-times-l"></i>
+									<i className="anm anm-bars-r"></i>
+								</button>
+							</div>
+							<nav className="grid__item" id="AccessibleNav">
+								<ul id="siteNav" className="site-nav medium center hidearrow">
+									<li className="lvl1 parent megamenu">
+										<a href="#">
+											Home <i className="anm anm-angle-down-l"></i>
+										</a>
+									</li>
+									<li className="lvl1 parent megamenu">
+										<a href="#">
+											Shop <i className="anm anm-angle-down-l"></i>
+										</a>
+									</li>
+									<li className="lvl1 parent megamenu">
+										<a href="#">
+											Product <i className="anm anm-angle-down-l"></i>
+										</a>
+									</li>
+									<li className="lvl1 parent dropdown">
+										<a href="#">
+											Pages <i className="anm anm-angle-down-l"></i>
+										</a>
+										<ul className="dropdown">
+											<li>
+												<a href="cart-variant1.html" className="site-nav">
+													Cart Page <i className="anm anm-angle-right-l"></i>
+												</a>
+												<ul className="dropdown">
+													<li>
+														<a href="cart-variant1.html" className="site-nav">
+															Cart Variant1
+														</a>
+													</li>
+													<li>
+														<a href="cart-variant2.html" className="site-nav">
+															Cart Variant2
+														</a>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<a href="compare-variant1.html" className="site-nav">
+													Compare Product{" "}
+													<i className="anm anm-angle-right-l"></i>
+												</a>
+												<ul className="dropdown">
+													<li>
+														<a
+															href="compare-variant1.html"
+															className="site-nav"
+														>
+															Compare Variant1
+														</a>
+													</li>
+													<li>
+														<a
+															href="compare-variant2.html"
+															className="site-nav"
+														>
+															Compare Variant2
+														</a>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<a href="checkout.html" className="site-nav">
+													Checkout
+												</a>
+											</li>
+											<li>
+												<a href="about-us.html" className="site-nav">
+													About Us <span className="lbl nm_label1">New</span>{" "}
+												</a>
+											</li>
+											<li>
+												<a href="contact-us.html" className="site-nav">
+													Contact Us
+												</a>
+											</li>
+											<li>
+												<a href="faqs.html" className="site-nav">
+													FAQs
+												</a>
+											</li>
+											<li>
+												<a href="lookbook1.html" className="site-nav">
+													Lookbook<i className="anm anm-angle-right-l"></i>
+												</a>
+												<ul>
+													<li>
+														<a href="lookbook1.html" className="site-nav">
+															Style 1
+														</a>
+													</li>
+													<li>
+														<a href="lookbook2.html" className="site-nav">
+															Style 2
+														</a>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<a href="404.html" className="site-nav">
+													404
+												</a>
+											</li>
+											<li>
+												<a href="coming-soon.html" className="site-nav">
+													Coming soon <span className="lbl nm_label1">New</span>{" "}
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="lvl1 parent dropdown">
+										<a href="#">
+											Blog <i className="anm anm-angle-down-l"></i>
+										</a>
+										<ul className="dropdown">
+											<li>
+												<a href="blog-left-sidebar.html" className="site-nav">
+													Left Sidebar
+												</a>
+											</li>
+											<li>
+												<a href="blog-right-sidebar.html" className="site-nav">
+													Right Sidebar
+												</a>
+											</li>
+											<li>
+												<a href="blog-fullwidth.html" className="site-nav">
+													Fullwidth
+												</a>
+											</li>
+											<li>
+												<a href="blog-grid-view.html" className="site-nav">
+													Gridview
+												</a>
+											</li>
+											<li>
+												<a href="blog-article.html" className="site-nav">
+													Article
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li className="lvl1">
+										<a href="#">
+											<b>Buy Now!</b> <i className="anm anm-angle-down-l"></i>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<div className="col-4 col-sm-3 col-md-3 col-lg-2">
+							<div className="site-cart">
+								<a href="#;" className="site-header__cart" title="Cart">
+									<i className="icon anm anm-bag-l"></i>
+									<span
+										id="CartCount"
+										className="site-header__cart-count"
+										data-cart-render="item_count"
+									>
+										2
+									</span>
+								</a>
+								<div id="header-cart" className="block block-cart">
+									<ul className="mini-products-list">
+										<li className="item">
+											<a className="product-image" href="#">
+												<img
+													src="assets/images/product-images/cape-dress-1.jpg"
+													alt="3/4 Sleeve Kimono Dress"
+													title=""
+												/>
+											</a>
+											<div className="product-details">
+												<a href="#" className="remove">
+													<i className="anm anm-times-l" aria-hidden="true"></i>
+												</a>
+												<a href="#" className="edit-i remove">
+													<i className="anm anm-edit" aria-hidden="true"></i>
+												</a>
+												<a className="pName" href="cart.html">
+													Sleeve Kimono Dress
+												</a>
+												<div className="variant-cart">Black / XL</div>
+												<div className="wrapQtyBtn">
+													<div className="qtyField">
+														<span className="label">Qty:</span>
+														<a className="qtyBtn minus" href="">
+															<i
+																className="fa anm anm-minus-r"
+																aria-hidden="true"
+															></i>
+														</a>
+														<input
+															type="text"
+															className="product-form__input qty"
+														/>
+														<a className="qtyBtn plus" href="">
+															<i
+																className="fa anm anm-plus-r"
+																aria-hidden="true"
+															></i>
+														</a>
+													</div>
+												</div>
+												<div className="priceRow">
+													<div className="product-price">
+														<span className="money">$59.00</span>
+													</div>
+												</div>
+											</div>
+										</li>
+										<li className="item">
+											<a className="product-image" href="#">
+												<img
+													src="assets/images/product-images/cape-dress-2.jpg"
+													alt="Elastic Waist Dress - Black / Small"
+													title=""
+												/>
+											</a>
+											<div className="product-details">
+												<a href="#" className="remove">
+													<i className="anm anm-times-l" aria-hidden="true"></i>
+												</a>
+												<a href="#" className="edit-i remove">
+													<i className="anm anm-edit" aria-hidden="true"></i>
+												</a>
+												<a className="pName" href="cart.html">
+													Elastic Waist Dress
+												</a>
+												<div className="variant-cart">Gray / XXL</div>
+												<div className="wrapQtyBtn">
+													<div className="qtyField">
+														<span className="label">Qty:</span>
+														<a className="qtyBtn minus" href="">
+															<i
+																className="fa anm anm-minus-r"
+																aria-hidden="true"
+															></i>
+														</a>
+														<input
+															type="text"
+															className="product-form__input qty"
+														/>
+														<a className="qtyBtn plus" href="">
+															<i
+																className="fa anm anm-plus-r"
+																aria-hidden="true"
+															></i>
+														</a>
+													</div>
+												</div>
+												<div className="priceRow">
+													<div className="product-price">
+														<span className="money">$99.00</span>
+													</div>
+												</div>
+											</div>
+										</li>
+									</ul>
+									<div className="total">
+										<div className="total-in">
+											<span className="label">Cart Subtotal:</span>
+											<span className="product-price">
+												<span className="money">$748.00</span>
+											</span>
+										</div>
+										<div className="buttonSet text-center">
+											<a
+												href="cart.html"
+												className="btn btn-secondary btn--small"
+											>
+												View Cart
+											</a>
+											<a
+												href="checkout.html"
+												className="btn btn-secondary btn--small"
+											>
+												Checkout
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="site-header__search">
+								<button type="button" className="search-trigger">
+									<i className="icon anm anm-search-l"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
